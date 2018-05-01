c0714963514fb04ec6f285ce885df9b1f4b2375e
DuyDo <duydo.dev@gmail.com>
2018-05-01 16:23:13 -0500

    finish project1

--- src/App.js
+++ src/App.js
@@ -27,8 +27,12 @@ class BooksApp extends React.Component {
     })
   }
 
+
   updateShelf = (book, shelf) => {
-    BooksAPI.update(book, shelf);
+    BooksAPI.update(book, shelf).then(() => {
+      book.shelf = shelf;
+      this.setState((currentState) => ({books: currentState.books.filter(b => b.id !== book.id).concat([book])}))
+    })
   }
 
   searchBook = (query) => {
